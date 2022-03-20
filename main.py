78b4b3d7e7fb75239df566bd4e7e87655238fdee
schweiny-08 <andrew.magri@outlook.com>
2022-03-20 20:15:06 +0100

    Added simple camera recording script

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 from utils.robot.Movement_Controls import Movement_Controls
 import RPi.GPIO as GPIO
 
-movement_controls = Movement_Controls()
+from camera_record import record_video
 
-speed = 40
+record_time = 20
 
-while 1:
-    x = input()
-    speed = movement_controls.get_speed()
+from multiprocessing import Process
 
-    if x == 'w':
-        movement_controls.forward()
-    elif x == 's':
-        movement_controls.backward()
-    elif x == 'a':
-        movement_controls.left()
-    elif x == 'd':
-        movement_controls.right()
-    elif x == 'e':
-        if speed < 80:
-            speed += 5
-            movement_controls.change_speed(speed)
+cam_record = Process(target=record_video, args=(record_time))
+cam_record.start()
+cam_record.join()
+cam_record.close()
+
+# movement_controls = Movement_Controls()
+
+# speed = 40
+
+# while 1:
+#     x = input()
+#     speed = movement_controls.get_speed()
+
+#     if x == 'w':
+#         movement_controls.forward()
+#     elif x == 's':
+#         movement_controls.backward()
+#     elif x == 'a':
+#         movement_controls.left()
+#     elif x == 'd':
+#         movement_controls.right()
+#     elif x == 'e':
+#         if speed < 80:
+#             speed += 5
+#             movement_controls.change_speed(speed)
         
-    elif x == 'q':
-        if speed > 10:
-            speed -= 5
-            movement_controls.change_speed(speed)
-            print("Speed is " + speed.__str__())
-        else:
-            print("cannot go slower")
-    elif x == 'z':
-        print("exit")
-        GPIO.cleanup()
-        print("GPIO Clean Up")
-        break
-    elif x == 'r':
-        print("run")
-        movement_controls.run()
-    else:
-        print("BAD INPUT")
+#     elif x == 'q':
+#         if speed > 10:
+#             speed -= 5
+#             movement_controls.change_speed(speed)
+#             print("Speed is " + speed.__str__())
+#         else:
+#             print("cannot go slower")
+#     elif x == 'z':
+#         print("exit")
+#         GPIO.cleanup()
+#         print("GPIO Clean Up")
+#         break
+#     elif x == 'r':
+#         print("run")
+#         movement_controls.run()
+#     else:
+#         print("BAD INPUT")
---
+++ utils/robot/camera_record.py
@@ -0,0 +1,44 @@
+import cv2
+from video_capture import VideoCaptureAsync
+import time
+
+vid_w = 1080
+vid_h = 720
+
+capture = VideoCaptureAsync(0, vid_w, vid_h)
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+def record_video(duration):
+    capture.start()
+    time_end = time.time() + duration
+
+    frames = 0
+    images = []
+
+    # This loop only captures time for above duration
+    while time.time() <= time_end:
+        ret, new_frame = capture.read()
+        images.append(new_frame)
+        
+        # Creates the display
+        cv2.namedWindow('image', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        
+        # Shows every 5th frame to the display
+        if frames == 0 or frame%5 == 0:
+            frame = cv2.flip(frame, 180)
+            cv2.imshow('frame', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    capture.stop()
+    cv2.destroyAllWindows()
+
+    fps = frames/duration
+
+    print(frames)
+    print(fps)
+    print(len(images))
+
+    
