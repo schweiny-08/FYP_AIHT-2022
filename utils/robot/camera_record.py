2f6084b0ce6dee94300cd904fcd6807ea25ed773
schweiny-08 <andrew.magri@outlook.com>
2022-03-22 03:10:29 +0100

    fixed some bugs related to cam rec, works now stand alone

--- utils/robot/camera_record.py
+++ utils/robot/camera_record.py
@@ -1,12 +1,15 @@
 import cv2
 
-cap = cv2.VideoCapture()
-cap.open(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) and input() == 'z':
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
