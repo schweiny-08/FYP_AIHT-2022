80e55f1cd7d40949d1a4fefa7a894542036d86e5
schweiny-08 <andrew.magri@outlook.com>
2022-03-22 03:01:03 +0100

    added waitKey

--- utils/robot/camera_record.py
+++ utils/robot/camera_record.py
@@ -6,7 +6,7 @@ cap.open(0, cv2.CAP_DSHOW)
 while True:
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    if input() == 'z':
+    if cv2.waitKey(1) and input() == 'z':
         break
 
 cap.release()
