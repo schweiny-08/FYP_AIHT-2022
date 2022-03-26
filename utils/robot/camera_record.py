f088eadaba18993c5a4c4821578f0337650500b7
schweiny-08 <andrew.magri@outlook.com>
2022-03-26 19:48:22 +0100

    using process in subclass

--- utils/robot/camera_record.py
+++ utils/robot/camera_record.py
@@ -1,5 +1,5 @@
 import cv2
-import threading
+from multiprocessing import Process
 
 
 class Camera_Record:
@@ -10,19 +10,20 @@ class Camera_Record:
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    
+
+    def start_camera(self):
+        self.camera_thread = Process(target=self.start_camera_thread)
+        self.camera_thread.start()
+
     def start_camera_thread(self):
         while True:
             ret, frame = self.cap.read()
             cv2.imshow('frame', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-
-    def start_camera(self):
-        self.camera_thread = threading.Thread(target=start_camera_thread)
-        self.camera_thread.start()
-
-   
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
     def stop_camera(self):
-        # self.camera_thread.
+        self.camera_thread.kill()
         self.cap.release()
         cv2.destroyAllWindows()
