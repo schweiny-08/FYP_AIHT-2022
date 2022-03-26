8069aaea2b844ebb9d2e65b2a7c0cd54dff82642
schweiny-08 <andrew.magri@outlook.com>
2022-03-26 14:09:06 +0100

    added subprocess try

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
 from utils.robot.Movement_Controls import Movement_Controls
-import RPi.GPIO as GPIO
+import utils.robot.camera_record as cam_script
 
-from utils.robot.camera_record import record_video
+import RPi.GPIO as GPIO
+import concurrent.futures
+
 
 record_time = 20
 
-from multiprocessing import Process
-
-# cam_record = Process(target=record_video)
-# cam_record.start()
-# cam_record.join()
-# cam_record.close()
+# from multiprocessing import Process
 
 movement_controls = Movement_Controls()
 
 speed = 40
 
-while 1:
 
-    cam_record.start()
+with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
 
-    x = input()
-    speed = movement_controls.get_speed()
+    executor.map(cam_script)
 
-    if x == 'w':
-        movement_controls.forward()
-    elif x == 's':
-        movement_controls.backward()
-    elif x == 'a':
-        movement_controls.left()
-    elif x == 'd':
-        movement_controls.right()
-    elif x == 'e':
-        if speed < 80:
-            speed += 5
-            movement_controls.change_speed(speed)
-        
-    elif x == 'q':
-        if speed > 10:
-            speed -= 5
-            movement_controls.change_speed(speed)
-            print("Speed is " + speed.__str__())
+    while 1:
+
+        cam_record.start()
+
+        x = input()
+        speed = movement_controls.get_speed()
+
+        if x == 'w':
+            movement_controls.forward()
+        elif x == 's':
+            movement_controls.backward()
+        elif x == 'a':
+            movement_controls.left()
+        elif x == 'd':
+            movement_controls.right()
+        elif x == 'e':
+            if speed < 80:
+                speed += 5
+                movement_controls.change_speed(speed)
+
+        elif x == 'q':
+            if speed > 10:
+                speed -= 5
+                movement_controls.change_speed(speed)
+                print("Speed is " + speed.__str__())
+            else:
+                print("cannot go slower")
+        elif x == 'z':
+            print("exit")
+            GPIO.cleanup()
+            print("GPIO Clean Up")
+            executor.shutdown()
+            print("Subprocess Shutdown")
+            break
+        elif x == 'r':
+            print("run")
+            movement_controls.run()
         else:
-            print("cannot go slower")
-    elif x == 'z':
-        print("exit")
-        GPIO.cleanup()
-        cam_record.close()
-        print("GPIO Clean Up")
-        break
-    elif x == 'r':
-        print("run")
-        movement_controls.run()
-    else:
-        print("BAD INPUT")
+            print("BAD INPUT")
