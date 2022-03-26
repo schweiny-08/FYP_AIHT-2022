265a5608e1d976c173f04ebcef3eff96c58a55bd
Andrew Magri <40473641+schweiny-08@users.noreply.github.com>
2022-03-26 20:00:10 +0100

    Subrprocess try 1 (#1)
    * checking movement is still working
    
    * using thread instead of process
    
    * fixed typo
    
    * subthreading try 2
    
    * import work around try
    
    * trying with threads running in sub class
    
    * using process in subclass
    
    * main scipt controls subprocesses

--- main.py
+++ main.py
@@ -1,59 +1,57 @@
 from utils.robot.Movement_Controls import Movement_Controls
-import utils.robot.camera_record as cam_script
-
 import RPi.GPIO as GPIO
-import concurrent.futures
-
+from multiprocessing import Process
+from utils.robot.camera_record import Camera_Record
 
 record_time = 20
 
 # from multiprocessing import Process
 
 movement_controls = Movement_Controls()
+camera_record = Camera_Record()
 
 speed = 40
 
+#camera_record.start_camera()
+camera_process = Process(target=camera_record.start_camera_thread)
+camera_process.start()
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
 
-    executor.map(cam_script)
+while 1:
 
-    while 1:
+    x = input()
+    speed = movement_controls.get_speed()
 
-        cam_record.start()
-
-        x = input()
-        speed = movement_controls.get_speed()
-
-        if x == 'w':
-            movement_controls.forward()
-        elif x == 's':
-            movement_controls.backward()
-        elif x == 'a':
-            movement_controls.left()
-        elif x == 'd':
-            movement_controls.right()
-        elif x == 'e':
-            if speed < 80:
-                speed += 5
-                movement_controls.change_speed(speed)
-
-        elif x == 'q':
-            if speed > 10:
-                speed -= 5
-                movement_controls.change_speed(speed)
-                print("Speed is " + speed.__str__())
-            else:
-                print("cannot go slower")
-        elif x == 'z':
-            print("exit")
-            GPIO.cleanup()
-            print("GPIO Clean Up")
-            executor.shutdown()
-            print("Subprocess Shutdown")
-            break
-        elif x == 'r':
-            print("run")
-            movement_controls.run()
+    if x == 'w':
+        movement_controls.forward()
+    elif x == 's':
+        movement_controls.backward()
+    elif x == 'a':
+        movement_controls.left()
+    elif x == 'd':
+        movement_controls.right()
+    elif x == 'e':
+        if speed < 80:
+            speed += 5
+            movement_controls.change_speed(speed)
+        
+    elif x == 'q':
+        if speed > 10:
+            speed -= 5
+            movement_controls.change_speed(speed)
+            print("Speed is " + speed.__str__())
         else:
-            print("BAD INPUT")
+            print("cannot go slower")
+    elif x == 'z':
+        print("exit")
+        GPIO.cleanup()
+        print("GPIO Clean Up")
+        camera_process.kill()
+        camera_record.stop_camera()
+        print("Stopped camera")
+        break
+    elif x == 'r':
+        print("run")
+        movement_controls.run()
+    else:
+        print("BAD INPUT")
--- utils/robot/camera_record.py
+++ utils/robot/camera_record.py
@@ -1,16 +1,29 @@
 import cv2
+from multiprocessing import Process
 
-cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+class Camera_Record:
 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-cap.release()
-cv2.destroyAllWindows()
+    
+
+    def start_camera(self):
+        self.camera_thread = Process(target=self.start_camera_thread)
+        self.camera_thread.start()
+
+    def start_camera_thread(self):
+        while True:
+            ret, frame = self.cap.read()
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+    def stop_camera(self):
+        #self.camera_thread.kill()
+        self.cap.release()
+        cv2.destroyAllWindows()
