25e39a2e3cafdf1c838b8a358ce949354d012b4d
Andrew Magri <40473641+schweiny-08@users.noreply.github.com>
2022-06-26 21:06:36 +0200

    Object identification preprocessing try on pc (#4)
    * grayscale try
    
    * adding stuff I did before resuming

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
-from utils.robot.Movement_Controls import Movement_Controls
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 from multiprocessing import Process
+
+# from utils.robot.Movement_Controls import Movement_Controls
 from utils.robot.camera_record import Camera_Record
 
-record_time = 20
-
-# from multiprocessing import Process
-
-movement_controls = Movement_Controls()
+# movement_controls = Movement_Controls()
 camera_record = Camera_Record()
 
 speed = 40
 
-#camera_record.start_camera()
 camera_process = Process(target=camera_record.start_camera_thread)
 camera_process.start()
 
+# while 1:
 
-while 1:
+#     x = input()
+#     speed = movement_controls.get_speed()
 
-    x = input()
-    speed = movement_controls.get_speed()
-
-    if x == 'w':
-        movement_controls.forward()
-    elif x == 's':
-        movement_controls.backward()
-    elif x == 'a':
-        movement_controls.left()
-    elif x == 'd':
-        movement_controls.right()
-    elif x == 'e':
-        if speed < 80:
-            speed += 5
-            movement_controls.change_speed(speed)
+#     if x == 'w':
+#         movement_controls.forward()
+#     elif x == 's':
+#         movement_controls.backward()
+#     elif x == 'a':
+#         movement_controls.left()
+#     elif x == 'd':
+#         movement_controls.right()
+#     elif x == 'e':
+#         if speed < 80:
+#             speed += 5
+#             movement_controls.change_speed(speed)
         
-    elif x == 'q':
-        if speed > 10:
-            speed -= 5
-            movement_controls.change_speed(speed)
-            print("Speed is " + speed.__str__())
-        else:
-            print("cannot go slower")
-    elif x == 'z':
-        print("exit")
-        GPIO.cleanup()
-        print("GPIO Clean Up")
-        camera_process.kill()
-        camera_record.stop_camera()
-        print("Stopped camera")
-        break
-    elif x == 'r':
-        print("run")
-        movement_controls.run()
-    else:
-        print("BAD INPUT")
+#     elif x == 'q':
+#         if speed > 10:
+#             speed -= 5
+#             movement_controls.change_speed(speed)
+#             print("Speed is " + speed.__str__())
+#         else:
+#             print("cannot go slower")
+#     elif x == 'z':
+#         print("exit")
+#         GPIO.cleanup()
+#         print("GPIO Clean Up")
+#         camera_process.kill()
+#         camera_record.stop_camera()
+#         print("Stopped camera")
+#         break
+#     elif x == 'r':
+#         print("run")
+#         movement_controls.run()
+#     else:
+#         print("BAD INPUT")
--- testing_folder/testing_camera.py
+++ testing_folder/testing_camera.py
@@ -1,6 +1,66 @@
 import cv2
+import skimage
+import numpy as np
+from skimage.filters.thresholding import threshold_otsu
+from skimage import io
+
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+def obstacle_map(img, outer_iteration, inner_iteration=0):
+    current_image = img
+    object_map = np.zeros(img.shape)
+
+    for i in range(outer_iteration):
+        img_denoise = skimage.restoration.denoise_tv_chambolle(
+            current_image, max_num_iter=inner_iteration)
+        img_map = current_image - img_denoise
+        img_map[img_map < 0] = 0
+        object_map = object_map + img_map
+        current_image = img_denoise
+
+    current_image = 255 - img
+
+    for j in range(outer_iteration):
+        img_denoise = skimage.restoration.denoise_tv_chambolle(
+            current_image, max_num_iter=inner_iteration)
+        img_map = current_image - img_denoise
+        img_map[img_map < 0] = 0
+        object_map = object_map + img_map
+        current_image = img_denoise
+
+    return object_map
+
+
+def best_path(object_map, min_path_dist, min_path_width):
+    distance_map = np.ones(180)
+
 
 while True:
-    ret, image = cam.read()
-    
+    ret, raw_img = cam.read()
+    grayscale_img = skimage.color.rgb2gray(raw_img)
+    img_obstacle_map = obstacle_map(
+        img=grayscale_img, outer_iteration=8, inner_iteration=8)
+    denoised_img_obstacle_map = skimage.restoration.denoise_tv_chambolle(
+        img_obstacle_map)
+    otsu_threshold = threshold_otsu(denoised_img_obstacle_map)
+    object_binary = denoised_img_obstacle_map >= otsu_threshold
+    object_binary = 1.0*object_binary
+    # Todo: K connected
+    object_k_connected = skimage.measure.label(object_binary)
+
+    # io.imshow(object_k_connected)
+    # io.show()
+    # cv2.imshow("img", object_k_connected)
+
+    # object_binary = 1.0*object_binary
+    cv2.imshow("obs map1", denoised_img_obstacle_map)
+    cv2.imshow("obs map2", object_binary)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+self.cap.release()
+cv2.destroyAllWindows()
