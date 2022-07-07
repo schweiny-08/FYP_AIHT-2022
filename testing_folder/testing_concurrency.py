151f5be081bebb4c74c648c63f6dd29317a409ab
schweiny-08 <andrew.magri@outlook.com>
2022-07-07 19:24:20 +0200

    Camera object detection working - still need to integrate to movement

--- testing_folder/testing_concurrency.py
+++ testing_folder/testing_concurrency.py
@@ -1,4 +1,4 @@
-import threading
+from multiprocessing import Process
 
 def proc_1():
     while 1:
@@ -8,8 +8,8 @@ def proc_2():
         print("this is the thread 2")
 
 
-t1 = threading.Thread(target=proc_1)
-t2 = threading.Thread(target=proc_2)
+t1 = Process(target=proc_1)
+t2 = Process(target=proc_2)
 t1.start()
 t2.start()
 while 1:
